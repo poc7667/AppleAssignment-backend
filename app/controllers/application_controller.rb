c2ccdf351e32cc0184afe0de7f7ce4a2ccded335
WeiCheng Hsu <wei@cloudlendinginc.com>
2019-03-16 17:23:34 -0700

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format.json? }
+  protect_from_forgery with: :null_session
+  #, if: Proc.new { |c| c.request.format.json? }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   #   helper_method :current_user
